669f9470173160215c12760b6e98e99dd1052784
oak45985 <oak45985@gmail.com>
2022-10-08 23:34:50 -0500

    must continue working on contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function Contact() {
+
+    const [formState, setFormState] = useState({ name:"", email:"", message:"" });
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState
+
+    const contactSubmit = (e) => {
+        e.preventDefault();
+        if(!errorMessage) {
+            console.log('Form Submitted', formState);
+        }
+    };
+
+    const handleChange = (e) => {
+        if (!e.target.value.length) {
+            const
+        }
+    }
+
+    return(
+        <section>
+            <h1>Contact</h1>
+            <form id="contact-form" onSubmit={contactSubmit}>
+                <div>
+                    <label htmlFor='name'>Name:</label>
+                    <input name="name" type='text' defaultValue={name} onBlur={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor='email'>Email:</label>
+                    <input name='email' type='email' defaultValue={email} onBlur={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor='message'>Message:</label>
+                    <textarea name='message' rows='10' defaultValue={message} onBlur={handleChange} />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className='error'>{errorMessage}</p>
+                    </div>
+                )}
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    );
+};
+
+export default Contact;
