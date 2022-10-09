b2f99666f1421eff271c683241ec25d5b657ffbc
oak45985 <oak45985@gmail.com>
2022-10-08 21:06:17 -0500

    basic set up completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          This will be John's portfolio
+        </p>   
       </header>
+      <main>
+        <About></About>
+      </main>
+      <footer>
+        <p>This text will be footer items.</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <section>
+            <h1>This is about me information.</h1>
+            <div>
+                <p>This text will provide info about John.</p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
