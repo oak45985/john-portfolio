f8e4021f104c7eceed732d8562a151631e61d33a
oak45985 <oak45985@gmail.com>
2022-10-13 19:09:56 -0500

    updated resume download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import resume from '../../assets/resume/John Oakley Resume UPDATED amended.pdf'
-import ReactPDF from '@react-pdf/renderer';
+// import { PDFViewer } from '@react-pdf/renderer';
 
 const Resume = () => {
+
+    const onButtonClick = () => {
+        fetch(resume).then(response => {
+            response.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let link = document.createElement('a');
+                link.href = fileURL;
+                link.download = resume;
+                link.click();
+            })
+        })
+    }
+
     return(
         <section>
             <h1>Résumé</h1>
-            <p>resume info</p>
+            <iframe src={resume} title="resume" />
+            <button onClick={onButtonClick}>
+                Download Résumé
+            </button>
         </section>
     );
 }
